d238f81d144917abc58bbcbec185c92cc83c8efc
Jani Hyytiäinen <jani.hyytiainen@outlook.com>
2016-10-23 19:34:42 +0300

    Folder rename

--- tasks.ps1
+++ tasks.ps1
@@ -1,7 +1,7 @@
 function Nginx
 {
     Stop-Process -Name nginx -Force -ErrorAction SilentlyContinue
-    cd ut*
-    cd ng*
+    cd srv
+    cd nginx
     Start-Process -FilePath ./nginx.exe -ArgumentList '-c ../../conf/nginx.conf'
 }
